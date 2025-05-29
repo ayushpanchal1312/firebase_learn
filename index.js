3c02b3291cf864f9f533358131e10a1a4d3806c2
Ayush <Ayushpanchal1311@gmail.com>
2025-05-29 14:56:32 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import {signInWithEmailAndPassword,} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import {createUserWithEmailAndPassword ,} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
 import {auth} from "./firebase.js";
 
 document.getElementById("loginForm").addEventListener("submit", (event) => {
@@ -8,12 +10,24 @@ document.getElementById("loginForm").addEventListener("submit", (event) => {
   const password = document.getElementById("password").value;
 
   signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      console.log("Logged in:", userCredential.user);
+    .then(() => {
       location.href = "welcome.html"; // Redirect to welcome page
     })
     .catch((error) => {
-      console.error("Error:", error.code, error.message);
-      alert(`Login failed: ${error.message}`);
+      document.getElementById("error").innerHTML =error.message;
     });
 });
+
+document.getElementById('Signupbtn').addEventListener('click',(e)=>{
+  e.preventDefault();
+   const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then(() => {
+    location.href = "welcome.html"; // Redirect to welcome page
+  })
+  .catch((error) => {
+    document.getElementById("error").innerHTML = error.message;
+  });
+})
